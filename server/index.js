034a56a96d610e3850ed324bcb0ec86fc8b79e62
Sarah Lash <sarah.lash92@gmail.com>
2023-03-09 15:43:37 +0100

    updated port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = new Koa();
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const router = require('./router');
-const PORT = 4000;
+const PORT = 6000;
 
 app.use(cors());
 app.use(bodyParser());
