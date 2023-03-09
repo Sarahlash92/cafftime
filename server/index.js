334eadde2acb41a3cd3bfdd7a3bdb23fd5b322e4
Boryana Mincheva <bory.mincheva@gmail.com>
2023-03-09 14:39:02 +0000

    index port test

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const app = new Koa();
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const router = require('./router');
-const PORT = 4000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser());
 app.use(router.routes());
 
 
-app.listen(PORT, () => { console.log(`Server listening to http://localhost:${PORT}`) });
+app.listen(PORT, () => { console.log(`Server listening to http://localhost:${PORT}`); });
